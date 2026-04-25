d80a1e37fc6edc65219735eedb84a5e0c2afed9c
lachupacabra <dennisanand@hotmail.com>
2026-04-24 18:42:27 -0700

    initialize

--- 01_01b.py
+++ 01_01b.py
@@ -1,17 +1,19 @@
 def count_words(paragraph):
-    #code goes here
+    paragraph = paragraph.lower()
+    
+
     return
 
 def main():
-    paragraph = """Nadia’s Garden Restaurant is the creation of husband and wife team Nadia and Timothy Arbore. 
-    Their American-infused, Italian-based, organically created, cuisine was inspired by Nadia’s papa, an immigrant from Italy, 
-    who shared his love of cooking with Nadia as a young girl. His focus on using fresh ingredients and family style dining were 
-    the inspiration for Nadia’s Garden Restaurant. Located in the heart of Main Streets Historic District, they are proud to be a 
+    paragraph = """Nadia’s Garden Restaurant is the creation of husband and wife team Nadia and Timothy Arbore.
+    Their American-infused, Italian-based, organically created, cuisine was inspired by Nadia’s papa, an immigrant from Italy,
+    who shared his love of cooking with Nadia as a young girl. His focus on using fresh ingredients and family style dining were
+    the inspiration for Nadia’s Garden Restaurant. Located in the heart of Main Streets Historic District, they are proud to be a
     part of a rich community. In 2011, the duo remodeled the restaurant and updated their menu to include newer and more diverse entrées
-     that could be made from local organic suppliers. Preservation of the building’s original layout has allowed them to create smaller, 
+     that could be made from local organic suppliers. Preservation of the building’s original layout has allowed them to create smaller,
      more intimate, dining spaces. Nadia and Timothy are committed to sharing their family history of cuisine, along with their new inspirations,
       with their customers. Their passion for community, entertainment, and hospitality are found in every aspect of Nadia’s Garden Restaurant."""
-    print(count_words(paragraph)) 
+    print(count_words(paragraph))
 
 if __name__ == "__main__":
     main()
